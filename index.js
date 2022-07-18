ab6c8533ee9a160a0228b5673738a272cee66fa9
Lukmandst <lukmandwi11@gmail.com>
2022-07-18 22:33:51 +0700

    add provider, persist, container

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-/**
- * @format
- */
-
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Toast from 'react-native-toast-message';
+import 'react-native-gesture-handler';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './src/redux/store';
+
+import App from './src';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppWithRouter = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <App />
+        </NavigationContainer>
+        <Toast />
+      </SafeAreaProvider>
+    </PersistGate>
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => AppWithRouter);
