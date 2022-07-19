82fb8d352e22ec714854ab2499aefb8051328607
Lukmandst <lukmandwi11@gmail.com>
2022-07-20 03:59:06 +0700

    setting swr api

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './src/redux/store';
+import {SWRConfig} from 'swr';
+import {AppState} from 'react-native';
 
 import App from './src';
 import {name as appName} from './app.json';
@@ -16,7 +18,47 @@ const AppWithRouter = () => (
     <PersistGate loading={null} persistor={persistor}>
       <SafeAreaProvider>
         <NavigationContainer>
-          <App />
+          <SWRConfig
+            value={{
+              provider: () => new Map(),
+              isOnline() {
+                /* Customize the network state detector */
+                return true;
+              },
+              isVisible() {
+                /* Customize the visibility state detector */
+                return true;
+              },
+              initFocus(callback) {
+                let appState = AppState.currentState;
+
+                const onAppStateChange = nextAppState => {
+                  /* If it's resuming from background or inactive mode to active one */
+                  if (
+                    appState.match(/inactive|background/) &&
+                    nextAppState === 'active'
+                  ) {
+                    callback();
+                  }
+                  appState = nextAppState;
+                };
+
+                // Subscribe to the app state change events
+                const subscription = AppState.addEventListener(
+                  'change',
+                  onAppStateChange,
+                );
+
+                return () => {
+                  subscription.remove();
+                };
+              },
+              initReconnect(callback) {
+                /* Register the listener with your state provider */
+              },
+            }}>
+            <App />
+          </SWRConfig>
         </NavigationContainer>
         <Toast />
       </SafeAreaProvider>
---
+++ src/modules/api.js
@@ -0,0 +1,84 @@
+import useSWR from 'swr';
+import axios from 'axios';
+import {HOST_API} from '@env';
+const fetcher = (url, token) =>
+  axios
+    .get(url, {headers: {Authorization: `Bearer ${token}`}})
+    .then(res => res.data.data);
+const fetcher2 = url => axios.get(url).then(res => res.data.data);
+export function GetUser(token) {
+  const {data, error} = useSWR([`${HOST_API}/user/info`, token], fetcher, {
+    refreshInterval: 1000,
+  });
+  return {
+    user: data,
+    loadingUser: !error && !data,
+    isError: error,
+  };
+}
+export function GetFavProduct() {
+  const {data, error} = useSWR([`${HOST_API}/transaction/favorite`], fetcher2);
+  return {
+    Fav: data,
+    loadingFav: !error && !data,
+    isError: error,
+  };
+}
+export function GetAllProduct() {
+  const {data, error} = useSWR(
+    [`${HOST_API}/product/all?order=ASC&sort=name`],
+    fetcher2,
+  );
+  return {
+    All: data,
+    loadingAll: !error && !data,
+    isError: error,
+  };
+}
+export function GetCoffeeProduct() {
+  const {data, error} = useSWR(
+    [
+      `${HOST_API}/product?category=07e88ba9-1a54-46ab-bf2c-3dc8831090a4&order=ASC&sort=name`,
+    ],
+    fetcher2,
+  );
+  return {
+    Coffee: data,
+    loadingCoffee: !error && !data,
+    isError: error,
+  };
+}
+export function GetNonCoffeeProduct() {
+  const {data, error} = useSWR(
+    [
+      `${HOST_API}/product?category=30b95dde-a820-41dd-b474-902026e3e755&order=ASC&sort=name`,
+    ],
+    fetcher2,
+  );
+  return {
+    NonCoffee: data,
+    loadingNonCoffee: !error && !data,
+    isError: error,
+  };
+}
+export function GetFoodsProduct() {
+  const {data, error} = useSWR(
+    [
+      `${HOST_API}/product?category=ea71bfcd-f1f1-4976-ae1e-9ff0f2c70d0e&order=ASC&sort=name`,
+    ],
+    fetcher2,
+  );
+  return {
+    Foods: data,
+    loadingFoods: !error && !data,
+    isError: error,
+  };
+}
+export function GetInfoProduct(id) {
+  const {data, error} = useSWR([`${HOST_API}/product/${id}`], fetcher2);
+  return {
+    product: data,
+    loadingProduct: !error && !data,
+    isError: error,
+  };
+}
