86a6ece9f1fa7df72eb33944d1e549488c9c7e55
Lukmandst <lukmandwi11@gmail.com>
2022-07-26 01:38:15 +0700

    add splash screen

--- android/app/src/main/java/com/lukopinative/MainActivity.java
+++ android/app/src/main/java/com/lukopinative/MainActivity.java
@@ -3,12 +3,14 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
   // React Navigation
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+  SplashScreen.show(this);
   super.onCreate(null);
   }
 
